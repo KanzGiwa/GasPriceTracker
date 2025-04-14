1547abc0b2923317847004f9c5fe28f69467c295
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-04-14 22:51:07 +0000

    backenddd

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -126,7 +126,7 @@ void main() async {
         find.byKey(const ValueKey('LoginEmail_ws0q')), 'backendtest@gmail.com');
     await tester.enterText(
         find.byKey(const ValueKey('LoginPassword_yjz3')), 'Thisismypassword!');
-    await tester.tap(find.byKey(const ValueKey('SignupButton_7h7e')));
+    await tester.tap(find.byType(Button));
     await tester.pumpAndSettle(const Duration(milliseconds: 5000));
     expect(find.byKey(const ValueKey('Text_lqoy')), findsOneWidget);
   });
